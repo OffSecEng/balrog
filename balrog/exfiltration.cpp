d86c72ed8cf44731a0725adba74f639cadeaa035
Rajeev KARUVATH <razorrajeev@gmail.com>
2022-02-27 11:54:10 -0500

    refactor structure

--- balrog/exfiltration.cpp
+++ balrog/exfiltration.cpp
@@ -5,5 +5,8 @@
 using namespace std;
 
 void startExfiltration() {
+	char PATH_VARIABLE[1000];
+	GetEnvironmentVariableA("PATH", PATH_VARIABLE, 1000);
+	printf("PATH Variable : %s", PATH_VARIABLE);
 	printf("Started startExfiltration\n");
 }
